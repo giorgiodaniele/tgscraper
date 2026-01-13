5259a6ad54486c36fe6eaf7d292ed4446bfefa00
giorgiodaniele <giorgiodaniele15@gmail.com>
2026-01-13 16:30:54 +0100

    Fixed main.py to use older syntax for async main loop

--- src/main.py
+++ src/main.py
@@ -115,4 +115,7 @@ async def main():
     await client.disconnect()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())
+    loop.close()
